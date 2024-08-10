87c157f907c39e7d3d6507e24cf445719d34d763
DARPAN KUMAR SING <54790420+darpankr@users.noreply.github.com>
2024-08-10 15:08:41 +0000

    Intro commit

---
+++ Basics of Java/1. Introduction to java.java
@@ -0,0 +1,64 @@
+//This page is to know the basics
+
+/* 
+    JAVA BASICS ------->
+
+    > Java is an object oriented programming language
+    > It enable automatic memory management
+    > It also has multi threading
+    > Vast standard library
+    > Open Source
+    > Can make web and mobile application
+*/
+
+/*
+    HOW TO RUN JAVA PROGRAM IN CMD ---------->
+
+    > Install Java Devlopment Kit (JDK). [set up the JDK path in system environment variable]
+
+    STEPS:
+        1) You need to have jdk to run java program
+        2) You need text editor
+        3) write java code in editor & save it as .java extension
+        4) program: */ 
+        public class HelloWorld{
+            public static void main(String args[]){
+                system.out.println("Hello world");
+            }
+        } /*
+        5) Open cmd.
+        6) Use javac command to compile the code  => javac filename.java
+        7) similar way you can run java program in IDE
+
+
+
+        cmd:    javac filename.java  (compile it to byte code)
+                java classname  (class that contain main method)
+ */
+
+
+ /*
+ * 
+ *  FOLLOWING:
+ *  
+ *      1) Keep your java runtime environment upto date with security patches.
+ *      2) Secure coding
+ *      3) Secure communication protocols such as HTTPS.
+ */
+
+
+ /*
+
+    TYPES OF JAVA PROGRAM ---------->
+
+    1) Java Applet : Small java program that run within web browser.
+
+    2) Java Application: Stand alone programs. They run on a computer without the need of web browser.
+
+    3) Java Servlets: They run on web server. Handle HTTP request and response between client and server.
+
+    4) JavaServer Pager (JSP) : Server side technology. Help to create dynamic web pages using java.
+
+    5) JavaBeans: Reusable components for making java application.
+
+ */
